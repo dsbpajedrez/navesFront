be4c70f3c7ab1d0e5a6d8403023402ddce66ae11
David Santiago Benjumea Perez <davidbenjumchess@gmail.com>
2022-07-24 16:31:29 -0500

    new:initializing project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+  const [naves,setNaves] =useState({})
+  useEffect(()=>{
+    fetch('http://localhost:3001/tripulada')
+      .then(respuesta=>respuesta.json())
+      .then(data=> setNaves(data))
+  },[])
+  console.log(naves);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p></p>
     </div>
   );
 }
